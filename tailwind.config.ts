dac3999dc49adcd5def87f34c8e203ab6d38fbe3
pawelekbyra <pawelek.byra@gmail.com>
2026-05-23 18:13:39 +0200

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss"
+
+const config = {
+  content: [
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './lib/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("tailwindcss-animate"),
+  ],
+} satisfies Config
+
+export default config
